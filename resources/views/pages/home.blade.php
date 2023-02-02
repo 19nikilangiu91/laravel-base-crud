5061f63f8ef47299d11db3c9570fc99bf8e0967e
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-02 15:24:24 +0100

    Bonus 1 - Created foreach

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Content</h1>
+<h1>Saints</h1>
+<ul>
+    @foreach ($saints as $saint)
+        <li>
+            {{ $saint -> name }} {{ $saint -> place_of_birth }} - {{ $saint -> blessing_date }} - {{ $saint -> number_of_miracles }}
+        </li>
+    @endforeach
+</ul>
 @endsection
