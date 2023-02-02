b65a2aef32aa36c8ea66260c7fc8acd255aa4d46
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-02 15:50:18 +0100

    Bonus 2 - Added show function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,15 +9,30 @@ use App\Models\Saint;
 
 class MainController extends Controller
 {
+
+    // Bonus 1
     public function home()
     {
 
-        $saints = Saint::all();
+        $saint = Saint::all();
 
         $data = [
-            'saints' => $saints
+            'saints' => $saint
         ];
 
         return view('pages.home', $data);
+    }
+
+    // Bonus 2 
+    public function show($id)
+    {
+
+        $saint = Saint::find($id);
+
+        $data = [
+            'saint' => $saint
+        ];
+
+        return view('pages.saint', $data);
     }
 }
