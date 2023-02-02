ccdbf906fae35c948bec580a1f27fc2cb42e7f7d
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-02 15:53:23 +0100

    Bonus 1 & 2 - Removed row 9 and 10

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.main-layout')
 
 @section('content')
-<h1>Saints</h1>
-<ul>
-    @foreach ($saints as $saint)
-        <li>
-            Saint: {{ $saint -> name }} <br>
-            Place of Birth: {{ $saint -> place_of_birth }} <br>
-            Blessing Date: {{ $saint -> blessing_date }} <br>
-            Number of Miracles: {{ $saint -> number_of_miracles }}
-        </li>
-        <br>
-    @endforeach
-</ul>
+<div>
+    <h1>Saints</h1>
+    <ul>
+        @foreach ($saints as $saint)
+            <li>
+                <a href="/saint/{{ $saint -> id }}">
+                    Saint: {{ $saint -> name }} <br>
+                    Number of Miracles: {{ $saint -> number_of_miracles }}
+                </a>
+            </li>
+            <br>
+        @endforeach
+    </ul>
+</div>
+
 @endsection
