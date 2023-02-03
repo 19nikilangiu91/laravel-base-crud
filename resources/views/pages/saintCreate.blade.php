4f33ecc7ee6dd6469345ffea21a7957a4886d8c4
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-03 14:22:52 +0100

    Created saintCreated.blade.php

--- resources/views/pages/saintCreate.blade.php
+++ resources/views/pages/saintCreate.blade.php
@@ -3,5 +3,20 @@
 @section('content')
 <div>
     <h1 class="text">Create your Saint</h1>
+    <form>
+        <label for="name">Name</label>
+        <input type="text" name="name">
+        <br>
+        <label for="placeOfBirth">Place of Birth</label>
+        <input type="text" name="place_of_birth">
+        <br>
+        <label for="blessingDate">Blessing date</label>
+        <input type="date" name="blessing_date">
+        <br>
+        <label for="numberOfMiracle">Number Of Miracle</label>
+        <input type="number" name="number_of_miracles">
+        <br>
+        <input type="submit" value="Create you Saint">
+    </form>
 </div>
 @endsection 
